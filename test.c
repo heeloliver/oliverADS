60fb33aa7c2f68c8d5eecd4914ab68316e22716b
Oliver Mulcahy <omulcahy@ucsc.edu>
2020-09-01 18:55:18 -0600

    added hex formatting...for some reason

--- test.c
+++ test.c
@@ -10,6 +10,8 @@
 
 int main() {
 	char* hex = "8D4840D6202CC371C32CE0576098";
+	int hexLength = strlen(hex);
+	char hexProperlyFormatted[hexLength + 3];
 	char dfChunk[DFCHUNKLENGTH + 1];
 	char caChunk[CALENGTH + 1];
 	char icaoChunk[ICAOLENGTH + 1];
@@ -23,6 +25,15 @@ int main() {
 	int data;
 	int tc;
 	int pi;
+
+	printf("hex length: %d\n", hexLength);
+	hexProperlyFormatted[0] = '0';
+	hexProperlyFormatted[1] = 'x';
+	for (int i = 2; i < hexLength + 2; i++)
+	{
+		hexProperlyFormatted[i] = hex[i-2];
+	}
+	printf("hexProperlyFormatted: %s\n", hexProperlyFormatted);
 	
 	for (int i = 0; i < DFCHUNKLENGTH; i++)
 	{
