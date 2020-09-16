913695376b2c91419933ae78dec397258e083ae0
Oliver Mulcahy <omulcahy@ucsc.edu>
2020-09-15 18:18:12 -0700

    added header file

---
+++ ads.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DFCHUNKLENGTH 2
+#define CALENGTH 3
+#define ICAOLENGTH 24
+#define DATALENGTH 56
+#define PILENGTH 24 
+
+#define AIRCRAFTIDEN 0
+#define SURFACEPOS 1
+#define AIRBORNEPOSBARO 2
+#define AIRBORNEVELO 3
+#define AIRBORNEPOSGNSS 4
+#define RESERVED 5
+#define AIRCRAFTSTAT 6
+#define TARGETSTATE 7
+#define AIRCRAFTOPSTAT 8
+#define UNKNOWN 9
+
--- test.c
+++ test.c
@@ -1,23 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//test.c
+//oliver mulcahy (opm@ucsc.edu)
 
-#define DFCHUNKLENGTH 2
-#define CALENGTH 3
-#define ICAOLENGTH 24
-#define DATALENGTH 56
-#define PILENGTH 24 
+#ifndef ADS_H
+#define ADS_H
 
-#define AIRCRAFTIDEN 0
-#define SURFACEPOS 1
-#define AIRBORNEPOSBARO 2
-#define AIRBORNEVELO 3
-#define AIRBORNEPOSGNSS 4
-#define RESERVED 5
-#define AIRCRAFTSTAT 6
-#define TARGETSTATE 7
-#define AIRCRAFTOPSTAT 8
-#define UNKNOWN 9
+#include "ads.h"
+
+#endif
 
 char decodeChar(uint8_t num)
 {
